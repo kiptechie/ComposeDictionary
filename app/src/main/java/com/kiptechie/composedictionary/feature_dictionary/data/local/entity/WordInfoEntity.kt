eea201b1616ab928ceccdf1b3ae3a259d7bfc5e7
Timothy Kipkorir Serem <theecodepoet@gmail.com>
2021-11-21 20:40:38 +0300

    Added word info entity

---
+++ app/src/main/java/com/kiptechie/composedictionary/feature_dictionary/data/local/entity/WordInfoEntity.kt
@@ -0,0 +1,24 @@
+package com.kiptechie.composedictionary.feature_dictionary.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.kiptechie.composedictionary.feature_dictionary.domain.models.Meaning
+import com.kiptechie.composedictionary.feature_dictionary.domain.models.WordInfo
+
+@Entity
+data class WordInfoEntity(
+    val word: String,
+    val phonetic: String,
+    val origin: String,
+    val meanings: List<Meaning>,
+    @PrimaryKey val id: Int? = null
+) {
+    fun toWordInfo(): WordInfo {
+        return WordInfo(
+            word = word,
+            phonetic = phonetic,
+            origin = origin,
+            meanings = meanings
+        )
+    }
+}
