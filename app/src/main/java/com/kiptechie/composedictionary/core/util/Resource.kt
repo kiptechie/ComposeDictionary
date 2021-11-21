8d7fbca116984299781bbd3e1bb2a7cd56fcb693
Timothy Kipkorir Serem <theecodepoet@gmail.com>
2021-11-21 20:39:31 +0300

    Added generic sealed resource class

---
+++ app/src/main/java/com/kiptechie/composedictionary/core/util/Resource.kt
@@ -0,0 +1,10 @@
+package com.kiptechie.composedictionary.core.util
+
+sealed class Resource<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+    class Loading<T>(data: T? = null) : Resource<T>(data = data)
+    class Success<T>(data: T?) : Resource<T>(data = data)
+    class Error<T>(message: String?, data: T? = null) : Resource<T>(data = data, message = message)
+}
